86f1e3ebaa8b3a5c0170e88d226dd1340702d0c5
@revoltcode <oddybassey@gmail.com>
2021-04-09 05:49:16 +0100

    adding a simple Java message POJO. closes #2

---
+++ src/main/java/com/revoltcode/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,20 @@
+package com.revoltcode.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -6703826490277916847L;
+    private UUID id;
+    private String message;
+}
